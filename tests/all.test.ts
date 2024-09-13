4ecbf412b153efd63415edc7b244b258b174d1be
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-09-13 08:39:01 +0300

    add tests

---
+++ tests/all.test.ts
@@ -0,0 +1,72 @@
+import "reflect-metadata";
+import { Injectable } from "../src/decorators/injectable";
+import { Container } from "../src/containers/container";
+import { LifeCycle } from "../src/enums/life-cycle.enum";
+import { Module } from "../src/decorators/module";
+
+//Singleton use case
+@Injectable({ lifeCycle: LifeCycle.Singleton })
+class TestService {
+  public sayHello() {
+    return "Hello";
+  }
+}
+
+@Injectable()
+class UserService {
+  constructor(private readonly testService: TestService) {}
+
+  public greetUser() {
+    return this.testService.sayHello();
+  }
+}
+
+//Transient use case
+@Injectable()
+class DatabaseService {
+  public dbName(): string {
+    return "Example Db.";
+  }
+}
+
+@Injectable({ lifeCycle: LifeCycle.Transient })
+class ConfigService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  public connectDatabase(): string {
+    return this.databaseService.dbName();
+  }
+}
+
+@Module({ providers: [ConfigService] })
+class ExampleModule {}
+
+describe("Coverage tests", () => {
+  it("Should create singleton instance", () => {
+    const instance = Container.get(TestService);
+    const instance2 = Container.get(TestService);
+
+    expect(instance).toBe(instance2); // these two must be same instances as singleton needs
+    expect(instance.sayHello()).toBe("Hello");
+  });
+
+  it("Should inject dependencies", () => {
+    const instance = Container.get(UserService);
+
+    expect(instance.greetUser()).toBe("Hello");
+  });
+
+  it("Should create transient instance", () => {
+    const instance = Container.get(ConfigService);
+
+    expect(instance.connectDatabase()).toBe("Example Db.");
+  });
+
+  it("Should load all modules and its providers", () => {
+    Container.loadModules(ExampleModule);
+    const instance = Container.get(ConfigService);
+
+    expect(instance).toBeDefined();
+    expect(instance.connectDatabase()).toBe("Example Db.");
+  });
+});
