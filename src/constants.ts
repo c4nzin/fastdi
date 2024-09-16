e6cb4efc90f6cbac49f088e18849d77fe7720b65
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-09-16 13:07:38 +0300

    fix

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const INJECTABLE = "injectable";
+export const PROVIDERS = "providers";
+export const DESIGN_PARAM_TYPES = "design:paramtypes";
+export const LIFE_CYCLE = "lifecycle";
---
+++ src/types/constructible.ts
@@ -0,0 +1,1 @@
+export type Constructible<T = any> = new (...args: any[]) => T;
